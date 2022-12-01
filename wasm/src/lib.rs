f4e8fcd967a05d2eb4f20a74315ee8308f417e34
yankydoo <yankydoo@users.noreply.github.com>
2022-12-01 11:15:10 +0100

    create contract from ping-pong-egld template

---
+++ wasm/src/lib.rs
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    ping_pong_egld
+    (
+        getActivationTimestamp
+        getDeadline
+        getMaxFunds
+        getPingAmount
+        getUserAddresses
+        getUserStatus
+        ping
+        pong
+        pongAll
+        pongAllLastUser
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
